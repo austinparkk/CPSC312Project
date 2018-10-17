48e3ef0cf0d73cbb18e4f1f1d33cf38bc1715bfb
bashara <msbasharah@gmail.com>
2018-10-17 15:41:25 -0700

    parser1.hs

--- parser1.hs
+++ parser1.hs
@@ -1,18 +1,30 @@
+import Data.Typeable
 csvArray = []
 
 -- splitsep (==',') "3,5,"  => ["3","5",""]
-splitsep sep [] = [[]]
-splitsep sep (h:t)
-    | sep h = []: splitsep sep t
+-- splitsep delimiter ignore list
+splitsep _ _ [] = [[]]
+splitsep sep ig (h:t)
+    | sep h = []: splitsep sep ig t
+    | ig h = splitsep sep ig t
     | otherwise = ((h:w):rest)
-                where w:rest = splitsep sep t
+                where w:rest = splitsep sep ig t
 
-             
+fls x = False
 readcsv filename =
   do
     file <- readFile filename
-    return [splitsep (==',') line| line <- splitsep (=='\n') file]
+    --let csvArray = [splitsep (==',') line| line <- splitsep (=='\n') file]
+    --print (typeOf csvArray)
+    let elems = [splitsep (==',') (`elem` "\160\r\65279") line| line <- splitsep (=='\n') fls file]
+    let res = user elems
+    return res
 
- 
+select_row r elems = elems !! r
+sum_row r elems = sum [read e :: Int | e <- tail (elems !! r)]
 
+---------
+
+go = readcsv "HateCrimesByRegion2016.csv"
+user  = sum_row 3
                
