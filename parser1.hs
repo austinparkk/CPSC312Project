31c2c03614ea890c1456d0c7143215fe08c747ae
Austin Park <j.aparkk@gmail.com>
2018-10-17 14:01:42 -0700

    first commit

---
+++ parser1.hs
@@ -0,0 +1,18 @@
+csvArray = []
+
+-- splitsep (==',') "3,5,"  => ["3","5",""]
+splitsep sep [] = [[]]
+splitsep sep (h:t)
+    | sep h = []: splitsep sep t
+    | otherwise = ((h:w):rest)
+                where w:rest = splitsep sep t
+
+             
+readcsv filename =
+  do
+    file <- readFile filename
+    return [splitsep (==',') line| line <- splitsep (=='\n') file]
+
+ 
+
+                
